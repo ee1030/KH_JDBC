c5d6ceff0adca09b323a2d5b263aa9a78e63b5b8
ee100 <sksguswoek1@gmail.com>
2020-11-13 13:50:31 +0900

    update : testJDBC1
    scott 예제 추가

---
+++ testJDBC1/src/com/kh/scott/model/dao/EmpDAO.java
@@ -0,0 +1,15 @@
+package com.kh.scott.model.dao;
+
+import java.util.List;
+
+import com.kh.scott.model.vo.Emp;
+
+// DAO 맨요맨아멘근육맨 예아
+public class EmpDAO {
+
+	public List<Emp> selectAll() {
+		
+		return null;
+	}
+
+}
